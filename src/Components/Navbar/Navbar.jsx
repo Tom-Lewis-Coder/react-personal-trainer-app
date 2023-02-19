03a8b91865e02786d3b4f21c9580cc4ae3761381
Tom Lewis <tomlewis139@gmail.com>
2023-02-19 14:56:42 +0000

    added navbar, styles, and toggle function

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+
+import './styles.scss'
+
+const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
+
+    return (
+        <nav className='app__navbar'>
+            <ul className='app__navbar-links'>
+                {['Home', 'About', 'Media', 'Blog', 'Booking'].map(item => (
+                    <li
+                        className='app-flex p-text'
+                        key={item}
+                    >
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className='app__navbar-menu'>
+                <HiMenuAlt4 onClick={() => setToggle(!toggle)} />
+
+                {toggle && (
+                    <div>
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul className='app__navbar-links'>
+                            {['Home', 'About', 'Media', 'Blog', 'Booking'].map(item => (
+                                <li
+                                    className='app-flex p-text'
+                                    key={item}
+                                >
+                                    <a href={`#${item}`}>{item}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
+            <div className='app__navbar-logo'>
+                TSL FITNESS
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
