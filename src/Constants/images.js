86b4069b240a186a3a90ec5bfa0c24e7f97d1738
Tom Lewis <tomlewis139@gmail.com>
2023-02-23 16:17:10 +0000

    added img change interval to header section

--- src/Constants/images.js
+++ src/Constants/images.js
@@ -1,7 +1,9 @@
 import TSLimg from '../Assets/TSLimg.jpg';
+import TSLimg2 from '../Assets/TSLimg2.jpg';
 
-const images = {
+const images = [
     TSLimg,
-}
+    TSLimg2
+]
 
 export default images
--- src/Container/Header/Header.jsx
+++ src/Container/Header/Header.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { images } from '../../Constants'
 
 import './Header.scss'
 
 const Header = () => {
+    const [headerImg, setHeaderImg] = useState(images[0])
+
+    useEffect(() => {
+        let count = 0
+        const timer = setInterval(() => {
+            count === 1 ? count = 0 : count = 1
+            setHeaderImg(images[count])
+        }, 6000)
+
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
+
     return (
         <div className='app__header app-flex' id='home'>
-            <div className='app__header-container app-flex'>
-                <div className='app__header-top app-flex'>
-                    <div className='app__header-top-badge'>
-                        TSL Fitness Inspirational Quote
-                    </div>
+            <img src={headerImg} alt='fitness' />
+            <div className='app__header-top app-flex'>
+                <div className='app__header-top-badge'>
+                    TSL Fitness Inspirational Quote
                 </div>
-                <div className='app__header-bottom app-flex'>
-                    <div className='app__header-bottom-sign-up'>
-                        Sign up now ..
-                    </div>
+            </div>
+            <div className='app__header-bottom app-flex'>
+                <div className='app__header-bottom-sign-up'>
+                    Sign up now ..
                 </div>
             </div>
         </div>
