0470bef60ea1e60fe33244cbe616466a8c5ffddc
Tom Lewis <tomlewis139@gmail.com>
2023-02-23 14:05:46 +0000

    updated header section to 100vh

--- src/Container/Header/Header.jsx
+++ src/Container/Header/Header.jsx
@@ -5,14 +5,16 @@ import './Header.scss'
 const Header = () => {
     return (
         <div className='app__header app-flex' id='home'>
-            <div className='app__header-top app-flex'>
-                <div className='app__header-top-badge'>
-                    TSL Fitness Inspirational Quote
+            <div className='app__header-container app-flex'>
+                <div className='app__header-top app-flex'>
+                    <div className='app__header-top-badge'>
+                        TSL Fitness Inspirational Quote
+                    </div>
                 </div>
-            </div>
-            <div className='app__header-bottom app-flex'>
-                <div className='app__header-bottom-sign-up'>
-                    Sign up now ..
+                <div className='app__header-bottom app-flex'>
+                    <div className='app__header-bottom-sign-up'>
+                        Sign up now ..
+                    </div>
                 </div>
             </div>
         </div>
