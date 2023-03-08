65c1e5a7f310dd59edfea5571a87e569d4df0771
Tom Lewis <tomlewis139@gmail.com>
2023-03-08 16:23:43 +0000

    updated color scheme, added to services section content

--- src/Container/Services/Services.jsx
+++ src/Container/Services/Services.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import './Services.scss'
+
 const Services = () => {
   return (
-    <div id='services'>Services</div>
+    <div className='app__services app__flex' id='services'>
+      <div className='app__services-title'>Programmes</div>
+      <div className='app__services-split-container app__flex'>
+        <div className='app__services-left'>
+          <div className='app__services-img-121'></div>
+          <div>1-2-1 Training in Person or Virtually</div>
+          <p>Dedicated, bespoke support from your Personal Trainer.</p>
+        </div>
+        <div className='app__services-right'>
+          <div className='app__services-img-stretch'></div>
+          <div>Group Training</div>
+          <p>Train with friends or family for extra motivation.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
