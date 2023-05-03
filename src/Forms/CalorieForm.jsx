722176a99d8f06ca8e43cd710cc23bdf0c11d5c3
Tom Lewis <tomlewis139@gmail.com>
2023-05-03 13:21:42 +0100

    styled calCalc form1, split radio functionilty out from setForm

--- src/Forms/CalorieForm.jsx
+++ src/Forms/CalorieForm.jsx
@@ -3,13 +3,14 @@ import Button from '../Components/Button'
 
 const CalorieForm = () => {
 
-    const [form1Data, setForm1Data] = useState({ age: '', gender: '', height: '', weight: '', activity: '' })
+    const [form1Data, setForm1Data] = useState({ age: '', height: '', weight: '', activity: '' })
     const [form2Data, setForm2Data] = useState({ weightGoal: '', timeFrame: '' })
     const [form1Result, setForm1Result] = useState(0)
     const [form2Result, setForm2Result] = useState(0)
+    const [gender, setGender] = useState('')
 
 
-    const { age, gender, height, weight, activity } = form1Data
+    const { age, height, weight, activity } = form1Data
     const { weightGoal, timeFrame } = form2Data
 
 
@@ -23,23 +24,25 @@ const CalorieForm = () => {
         setForm1Data({ ...form1Data, [name]: value })
     }
 
+    const getGender = e => {
+        setGender(e.target.value)
+    }
 
     const handleForm1Submit = e => {
         e.preventDefault()
-        if (age === '' || height === '' || weight === '' || gender === '' || activity === '') {
+        console.log(gender)
+        if (age === '' || gender === '' || height === '' || weight === '' || activity === '') {
             alert('Please ensure all fields are entered')
         } else {
-            let result1 = Math.floor(((weight * 10) + (height * 6.25) - (age * 5) + (gender === 'm' ? 5 : - 161)) * activity)
+            let result1 = Math.floor(((weight * 10) + (height * 6.25) - (age * 5) + (gender === 'male' ? 5 : - 161)) * activity)
             setForm1Result(result1)
         }
     }
-
 
     const handleForm2Change = e => {
         const { name, value } = e.target
         setForm2Data({ ...form2Data, [name]: value })
     }
-
 
     const handleForm2Submit = e => {
         e.preventDefault()
@@ -51,68 +54,63 @@ const CalorieForm = () => {
         }
     }
 
-
     return (
         <>
             <form className='app__calorie-form1'>
                 <div className='app__calCalc'>
-                    <label>Age</label>
+                    <label>Age </label>
                     <input
+                        className="app__age-input"
                         type="number"
                         name="age"
                         min="15"
                         max="80"
                         value={age}
                         onChange={handleForm1Change}
-                    />
-                    <label>15 - 80</label><br />
-                    <label>Gender</label><br />
+                    /><br />
                     <input
+                        id='male'
                         type="radio"
-                        name="gender"
-                        value='m'
-                        onChange={handleForm1Change}
-                    />
-                    <label>Male</label><br />
-
+                        name="radio"
+                        value='male'
+                        onChange={getGender}
+                    /><label className='app__gender-input' htmlFor='male'> Male</label>
                     <input
+                        id='female'
                         type="radio"
-                        name="gender"
-                        value='f'
-                        onChange={handleForm1Change}
-                    />
-                    <label>Female</label><br />
-
-                    <label>Height</label>
+                        name="radio"
+                        value='female'
+                        onChange={getGender}
+                    /><label className='app__gender-input' htmlFor='female'> Female</label>
+                    <label>Height </label>
                     <input
+                        className='app__height-input'
+                        placeholder='CM'
                         type="text"
                         name="height"
                         value={height}
                         onChange={handleForm1Change}
-                    />
-                    <label>cm</label><br />
-
-                    <label>Weight</label>
+                    /><br />
+                    <label>Weight </label>
                     <input
+                        className='app__weight-input'
+                        placeholder='KG'
                         type="text"
                         name="weight"
                         value={weight}
                         onChange={handleForm1Change}
-                    />
-                    <label>kg</label><br />
-
+                    /><br />
                     <label>Activity</label>
-                    <select name="activity" onChange={handleForm1Change} value={activity}>
-                        <option>Pick your activity level</option>
+                    <select name="activity" onChange={handleForm1Change} value={activity} className='app__activity-input'>
+                        <option>Choose activity level</option>
                         <option value="1">BMR (Basal Metabolic Rate) No activity</option>
                         <option value="1.2">Sedentary: little to no activity</option>
-                        <option value="1.375">Lightly Active: exercise 1/3 x per week</option>
-                        <option value="1.465">Moderate Activity: exercise 4/5 x per week</option>
+                        <option value="1.375">Light: exercise 1/3 x per week</option>
+                        <option value="1.465">Moderate: exercise 4/5 x per week</option>
                         <option value="1.55">Active: daily exercise or intense 3/4 x per week</option>
-                        <option value="1.725">Very Active: intense exercise 6/7 x per week</option>
-                        <option value="1.9">Extremely Active: intense exercise daily or physical job</option>
+                        <option value="1.725">Very: intense exercise 6/7 x per week</option>
+                        <option value="1.9">Extremely: intense exercise/job daily</option>
                     </select>
-
                     <div className="results-container hidden">
                         <p className="finalNumberStyling"></p>
                     </div>
@@ -120,7 +118,7 @@ const CalorieForm = () => {
                 <Button title={'Calculate'} onClick={handleForm1Submit} className={'btn-default'} />
             </form><br />
 
-        
+
             <div>
                 <form className='calorie-form2'>
                     <label>Weight Goal</label>
--- src/Forms/WorkoutForm.jsx
+++ src/Forms/WorkoutForm.jsx
@@ -10,21 +10,21 @@ const WorkoutForm = ( props ) => {
                     name='radio'
                     value='low' 
                     onChange={props.onChange}
-                /><label className='workoutRadio' htmlFor='low' >Low</label>                              
+                /><label className='app__workoutRadio' htmlFor='low' >Low</label>                              
                 <input 
                     id='medium'
                     type='radio'
                     name='radio' 
                     value='medium' 
                     onChange={props.onChange} 
-                /><label className='workoutRadio' htmlFor='medium' >Med</label>                   
+                /><label className='app__workoutRadio' htmlFor='medium' >Med</label>                   
                 <input
                     id='high'                       
                     type='radio' 
                     name='radio'
                     value='high' 
                     onChange={props.onChange} 
-                /><label className='workoutRadio' htmlFor='high' >High</label> 
+                /><label className='app__workoutRadio' htmlFor='high' >High</label> 
             <p>Complete one circuit 3 times or change each time for a variation. Rest 1 min between circuits</p>
         </form>
         )}
