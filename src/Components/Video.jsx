a51f654c2b9f49a390785d986ba903db843a7108
Tom Lewis <tomlewis139@gmail.com>
2023-05-21 15:49:28 +0100

    added basic video styling

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Video = ({ id, url, title }) => {
     return (
         <div className='app__video-component'>
-            <Link to={`https://www.youtube.com/watch?v=${id}`} className='video-link'>
+            <Link to={`https://www.youtube.com/watch?v=${id}`} className='app__video-link'>
                 <div className='app__video-style'>
                     <img src={url} alt='' className='app__video-img' />
-                    <div className='app__video-title'>{title}</div>
+                    <div className='app__video-component-title'>{title}</div>
                 </div>
             </Link>
         </div>
--- src/Pages/Videos/Videos.jsx
+++ src/Pages/Videos/Videos.jsx
@@ -1,35 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Video } from '../../Components'
+import { Video, Header } from '../../Components'
 
 import './Videos.scss'
+
+let query = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.REACT_APP_YOUTUBE_CHANNEL_ID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+
 
 export const Videos = () => {
 
     const [data, setData] = useState([])
 
-    const getData = async () => {
-        await axios
-            .get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.REACT_APP_YOUTUBE_CHANNEL_ID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
-            .then((res) => {
-                setData(res?.data?.items)
-            })
-    }
-
     useEffect(() => {
+        const getData = async () => {
+            const data = await axios.get(query)
+            setData(data?.data?.items)
+        }
         getData()
+            .catch(console.error)
     }, [])
 
     return (
-        <div>
-            {data?.map(vid => {
-                return <Video
-                            key={vid?.id?.videoId}
-                            id={vid?.id?.videoId}
-                            url={vid?.snippet?.thumbnails?.default?.url}
-                            title={vid?.snippet?.title}
-                        />
-            })}
+        <div className='app__video-section'>
+            <Header title={'Videos'} className={'app__video-title'} />
+            <div className='app__video-container app__flex'>
+                {data?.map(vid => {
+                    return <Video
+                        key={vid?.id?.videoId}
+                        id={vid?.id?.videoId}
+                        url={vid?.snippet?.thumbnails?.default?.url}
+                        title={vid?.snippet?.title}
+                    />
+                })}
+            </div>
         </div>
     )
 }
