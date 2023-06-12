defee6a4fa3a80f139fe76a46c5c3f91b79397aa
Tom Lewis <tomlewis139@gmail.com>
2023-06-12 14:56:19 +0100

    updated services

--- src/Container/Services/Services.jsx
+++ src/Container/Services/Services.jsx
@@ -8,15 +8,30 @@ const Services = () => {
     <div className='app__services app__flex' id='services'>
       <Header title={'Programmes'} className={'app__services-title'} />
       <div className='app__services-split-container app__flex'>
-        <div className='app__services-left'>
+        <div className='app__services-service'>
           <div className='app__services-img-121'></div>
-          <div>1-2-1 Training in Person or Virtually</div>
+          <div className='app__services-desc'>1-2-1 Training</div>
           <p>Dedicated, bespoke support from your Personal Trainer.</p>
         </div>
-        <div className='app__services-right'>
+        <div className='app__services-service'>
           <div className='app__services-img-stretch'></div>
-          <div>Group Training</div>
+          <div className='app__services-desc'>Group Training</div>
           <p>Train in a group with friends or family for extra motivation.</p>
+        </div>
+        <div className='app__services-service'>
+          <div className='app__services-img-121'></div>
+          <div className='app__services-desc'>Online coaching</div>
+          <p>Dedicated, bespoke support from your Personal Trainer.</p>
+        </div>
+        <div className='app__services-service'>
+          <div className='app__services-img-121'></div>
+          <div className='app__services-desc'>Corporate training</div>
+          <p>Dedicated, bespoke support from your Personal Trainer.</p>
+        </div>
+        <div className='app__services-service'>
+          <div className='app__services-img-121'></div>
+          <div className='app__services-desc'>Retirement training</div>
+          <p>Dedicated, bespoke support from your Personal Trainer.</p>
         </div>
       </div>
     </div>
