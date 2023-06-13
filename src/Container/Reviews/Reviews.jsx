011add123b940505592464c90e7949e934fd232c
Tom Lewis <tomlewis139@gmail.com>
2023-06-13 11:21:38 +0100

    added chevrons to reviews

--- src/Container/Reviews/Reviews.jsx
+++ src/Container/Reviews/Reviews.jsx
@@ -3,27 +3,39 @@ import React, { useState, useEffect } from 'react'
 import reviewList from '../../Constants/ReviewsList'
 import './Reviews.scss'
 import { Testamonials } from '../../Components'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 const Reviews = () => {
 
+    const [currentIndex, setCurrentIndex] = useState(0)
     const [review, setReview] = useState(reviewList[0])
-
 
     useEffect(() => {
         let count = 0
         const timer = setInterval(() => {
             count === reviewList.length - 1 ? count = 0 : count++
             setReview(reviewList[count])
+            setCurrentIndex(count)
         }, 6000)
-
         return () => {
             clearInterval(timer)
         }
     }, [])
 
+    const handleClick = (index) => {
+        setCurrentIndex(index)
+        setReview(reviewList[index])
+    }
+
     return (
-        <div id='reviews'>
-            {<Testamonials name={review.Name} content={review.Content} stars={review.Stars} />}
+        <div id='reviews' className='app__testamonial'>
+            <div onClick={() => handleClick(currentIndex === 0 ? reviewList.length - 1 : currentIndex - 1)}>
+                <FaChevronLeft size={32} />
+            </div>
+            <Testamonials name={review.Name} content={review.Content} stars={review.Stars} />
+            <div onClick={() => handleClick(currentIndex === reviewList.length - 1 ? 0 : currentIndex + 1)}>
+                <FaChevronRight size={32} />
+            </div>
         </div>
     )
 }
