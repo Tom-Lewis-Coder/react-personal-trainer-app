4114440e9f1a73c400a1da6142b961c5f3cd2a61
Tom Lewis <tomlewis139@gmail.com>
2023-06-21 21:04:34 +0100

    reverted

--- src/Constants/ServicesContent.jsx
+++ src/Constants/ServicesContent.jsx
@@ -1,9 +1,9 @@
-const ServicesContent = [ 
-`Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma, bar panna so Barista cortado trifecta extraction,skinny aftertaste filter java cultivar cinnamon.
-Mazagran trade Barista french and Acerbic acerbic Aged milk cinnamon origin carajillo, mountain coffee roast mug wings Bar singleviennese pumpkin go pot`, 
-`dripper crema flavour mocha At bar sit medium au breve.Espresso Brewed Blue iced Americano robust whipped, bar percolator grounds go saucer robusta, Au shop Affogato Bar aged coffee, 
+const ServicesContent = [
+    `Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma, bar panna so Barista cortado trifecta extraction,skinny aftertaste filter java cultivar cinnamon.
+Mazagran trade Barista french and Acerbic acerbic Aged milk cinnamon origin carajillo, mountain coffee roast mug wings Bar singleviennese pumpkin go pot`,
+    `dripper crema flavour mocha At bar sit medium au breve.Espresso Brewed Blue iced Americano robust whipped, bar percolator grounds go saucer robusta, Au shop Affogato Bar aged coffee, 
 Barista blue strong origin aftertaste. Blue skinny coffee breve Brewed acerbic, siphon steamed And foam, qui Arabica ut latte.`,
-`Go brewed At aftertaste sweet cinnamon caffeine rich strong caramelization Aftertaste, Body roast body frappuccino Beans extraction sit americano Aroma.`
+    `Go brewed At aftertaste sweet cinnamon caffeine rich strong caramelization Aftertaste, Body roast body frappuccino Beans extraction sit americano Aroma.`
 ]
 
 export default ServicesContent
