865e0fef0c671ea91a1ec3b7a4f9f6070ec11f0c
Tom Lewis <tomlewis139@gmail.com>
2023-07-18 18:09:09 +0100

    updated contact info

--- src/Container/Contact/Contact.jsx
+++ src/Container/Contact/Contact.jsx
@@ -14,15 +14,15 @@ const Contact = () => {
                 <ContactForm setIsFormSubmitted={setIsFormSubmitted} />
                 :
                 <div>
-                    <h3 className='app__contact-title-resp app__flex'>Thank you for getting in touch.</h3>
+                    <h3 className='app__contact-title-resp app__flex'>Thank you for getting in touch. We will be in contact shortly.</h3>
                 </div>
             }
             <div className='app__contact-direct app__flex'>
                 <div className='app__contact-title app__flex'>Contact Directly</div>
                 <div className='app__contact-card '>
-                    <a className='app__contact-card-item' href='mailto:tomlewis139@gmail.com'>
+                    <a className='app__contact-card-item' href='mailto:tslfitnessltd@gmail.com'>
                         <img src={images[0]} alt='email' />
-                        <div>tomlewis139@gmail.com</div>
+                        <div>tslfitnessltd@gmail.com</div>
                     </a>
                     <a className='app__contact-card-item' href='tel:07376 265677'>
                         <img src={images[1]} alt='mobile' />
